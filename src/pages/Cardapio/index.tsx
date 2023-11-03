04ea02429ba2dbf5a25c094d0079d2d454ff5ef2
Raykall <rayandro_22@hotmail.com>
2023-11-03 15:30:55 -0300

    finalizado a primeira parte

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import styles from './Cardapio.module.scss';
 import {ReactComponent as Logo} from 'assets/logofp.svg';
 export default  function Cardapio() {
@@ -14,3 +15,37 @@ export default  function Cardapio() {
         </main>
     )
 }
+=======
+import Buscador from "pages/Cardapio/Buscador";
+import styles from "./Cardapio.module.scss";
+import { ReactComponent as Logo } from "assets/logofp.svg";
+import { useState } from "react";
+import Filtros from "./Filtros";
+import Ordenador from "./Ordenador";
+import Itens from "./Itens";
+
+export default function Cardapio() {
+  const [busca, setBusca] = useState("");
+  const [filtro, setFiltro] = useState<number | null>(null);
+  const [ordenador, setOrdenador] = useState("");
+  return (
+    <main>
+      <nav className={styles.menu}>
+        <Logo />
+      </nav>
+      <header className={styles.header}>
+        <div className={styles.header__text}>A Loja Fit dos Sonhos!</div>
+      </header>
+      <section className={styles.cardapio}>
+        <h3 className={styles.cardapio__text}>Cardápio</h3>
+        <Buscador busca={busca} setBusca={setBusca} />
+        <div className={styles.cardapio__filtros}>
+        <Filtros filtro={filtro} setFiltro={setFiltro}/>
+        <Ordenador ordenador={ordenador} setOrdenador={setOrdenador}/>
+        </div>
+        <Itens busca={busca} filtro={filtro} ordenador={ordenador}/>
+      </section>
+    </main>
+  );
+}
+>>>>>>> dcfce0c (cardio, itens - item(final), projeto finalizado!)
